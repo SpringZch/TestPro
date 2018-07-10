2a0a2d23b4e3ad442316d85851463f82dac700f3
SpringZch <1484555271@qq.com>
2018-07-10 12:14:06 +0800

    r sdfei

---
+++ src/main/java/org/ch/TestPro/MainClassTest.java
@@ -0,0 +1,10 @@
+package org.ch.TestPro;
+
+/**
+ * Created by Administrator on 2018/7/9.
+ */
+public class MainClassTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
